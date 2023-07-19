2b13f68eacf5493227a16829979e5a8a02990382
ixury <mutiararuci97@gmail.com>
2023-07-19 19:34:14 +0700

    Add Food page & search bar

---
+++ Swise/View/AddFoodView.swift
@@ -0,0 +1,84 @@
+//
+//  AddFoodView.swift
+//  Swise
+//
+//  Created by Mutiara Ruci on 19/07/23.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    @Binding var text: String
+    
+       @State private var isEditing = false
+    
+       var body: some View {
+           VStack{
+               Text("Add Food").bold()
+                   .font(.title).padding(.trailing, 240)
+               HStack{
+                   TextField("Search ...", text: $text)
+                       .padding(7)
+                       .padding(.horizontal, 25)
+                       .background(Color(.systemGray6))
+                       .cornerRadius(8).overlay(
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading, 8)
+
+                            if isEditing {
+                                Button(action: {
+                                    self.text = ""
+                                }) {
+                                    Image(systemName: "multiply.circle.fill")
+                                        .foregroundColor(.gray)
+                                        .padding(.trailing, 8)
+                                }
+                            }
+                        }
+                    )
+                       .padding(.horizontal, 10)
+                       .onTapGesture {
+                           self.isEditing = true
+                       }
+        
+                   if isEditing {
+                       Button(action: {
+                           self.isEditing = false
+                           self.text = ""
+        
+                       }) {
+                           Text("Cancel")
+                       }
+                       .padding(.trailing, 10)
+                       .transition(.move(edge: .trailing))
+                       .animation(.default)
+                   }
+               }
+               
+               Divider().background(Color.gray.opacity(0.7))
+               
+               Button(
+                   action: {
+                       
+                   }){
+                       HStack{
+                           Image(systemName: "plus.circle")
+                           Text("Add your food manually").bold()
+                       }.foregroundColor(.white).frame(width: 358, height: 50).background(Color("button_color")).cornerRadius(11)
+                       
+                           
+                   }.padding()
+               
+           }.padding(.bottom, 490)
+         
+       }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView(text: .constant(""))
+    }
+}
--- Swise/View/SummaryView.swift
+++ Swise/View/SummaryView.swift
@@ -10,118 +10,119 @@ import SwiftUI
 struct SummaryView: View {
     @State var progressValue: Float = 0.65
     var body: some View {
-        ZStack{
-            ScrollView{
-                ZStack{
-                    VStack{
-                        HStack{
-                            Text("Today").bold().font(.system(size:34)).padding(.trailing, 200)
-                            Image(systemName: "info.circle").resizable().frame(width: 25, height: 25).background(Color.white).clipShape(Circle())
-                        }
-                        HStack{
-                            VStack{
-                                Text("You've Consumed")
-                                HStack{
-                                    Text("5 gr").bold().font(.system(size: 25))
-                                    Text("/50 gr").font(.system(size: 19)).padding(.top, 9)
-                                    Text("of sugar today!").padding(.top, 9)
-                                }
-                                Image("spoon")
-                                Text("or equals 1 teaspoon!")
-                                
-                            }.padding()
-                        }
-                        
-                    }
-                }
-                
-                VStack{
-                    Text("Calorie Count").font(.system(size: 22)).padding(.trailing,210)
-                }
-                ZStack{
-                    HStack{
-                        ProgressBar(progress: self.$progressValue)
-                            .frame(width: 80.0, height: 80.0)
-                            .padding(40.0)
-                        VStack(alignment: .leading){
+        NavigationView{
+            ZStack{
+                ScrollView{
+                    ZStack{
+                        VStack{
                             HStack{
-                                Text("56 kcal").foregroundColor(.black).bold().font(.system(size: 20))
-                                Text("/2200 kcal").font(.system(size: 14))
+                                Text("Today").bold().font(.system(size:34)).padding(.trailing, 200)
+                                Image(systemName: "info.circle").resizable().frame(width: 25, height: 25).background(Color.white).clipShape(Circle())
+                            }
+                            HStack{
+                                VStack{
+                                    Text("You've Consumed")
+                                    HStack{
+                                        Text("5 gr").bold().font(.system(size: 25))
+                                        Text("/50 gr").font(.system(size: 19)).padding(.top, 9)
+                                        Text("of sugar today!").padding(.top, 9)
+                                    }
+                                    Image("spoon")
+                                    Text("or equals 1 teaspoon!")
+                                    
+                                }.padding()
                             }
                             
-                            Text("*Based on your BMI, your max. calorie intake is 2200 kcal").foregroundColor(.black).font(.system(size: 12)).multilineTextAlignment(.leading).padding(.top, 5)
-                        }.padding(.trailing, 15)
-                        
+                        }
                     }
-                }.frame(width: 359, height: 124).background(Color("bg_blue")).cornerRadius(29)
-                
-                VStack{
-                    Text("Food Diary").font(.system(size: 22)).padding(.trailing,230)
-                }
-                ZStack{
+                    
+                    VStack{
+                        Text("Calorie Count").font(.system(size: 22)).padding(.trailing,210)
+                    }
+                    ZStack{
+                        HStack{
+                            ProgressBar(progress: self.$progressValue)
+                                .frame(width: 80.0, height: 80.0)
+                                .padding(40.0)
+                            VStack(alignment: .leading){
+                                HStack{
+                                    Text("56 kcal").foregroundColor(.black).bold().font(.system(size: 20))
+                                    Text("/2200 kcal").font(.system(size: 14))
+                                }
+                                
+                                Text("*Based on your BMI, your max. calorie intake is 2200 kcal").foregroundColor(.black).font(.system(size: 12)).multilineTextAlignment(.leading).padding(.top, 5)
+                            }.padding(.trailing, 15)
+                            
+                        }
+                    }.frame(width: 359, height: 124).background(Color("bg_blue")).cornerRadius(29)
+                    
+                    VStack{
+                        Text("Food Diary").font(.system(size: 22)).padding(.trailing,230)
+                    }
+                    ZStack{
+                        HStack{
+                            VStack{
+                                Text("🫐🧀").padding(.top, 10).foregroundColor(.black).bold().font(.system(size: 20))
+                                
+                                Text("What have you eaten today? Let us track here!").foregroundColor(.black).font(.system(size: 18)).multilineTextAlignment(.center).padding(.top, 1)
+                               NavigationLink(destination: AddFoodView(text: .constant(""))){
+                                        Text("Add your food")
+                                            .foregroundColor(.black).frame(width: 288, height: 46).background(Color("button_color")).cornerRadius(11)
+                                    }
+                                        
+                                
+                            }.padding(.trailing, 15)
+                            
+                        }
+                        
+                        
+                    }.frame(width: 359, height: 186).background(Color("bg_yellow")).cornerRadius(29)
+                    ZStack{
+                        VStack(alignment: .leading){
+                            HStack{
+                                Text("Bubur Ayam").padding(.trailing, 55)
+                                Text("17 kcal").padding(.trailing, 55)
+                                Text("2 gr")
+                            }.padding(.vertical, 0.4).bold().font(.system(size:17))
+                            
+                            Text("1 bowl")
+                        }
+                    }.frame(width: 356, height: 69.63).background(Color("bg_yellow")).cornerRadius(15)
+                    ZStack{
+                        VStack(alignment: .leading){
+                            HStack{
+                                Text("Bubur Ayam").padding(.trailing, 55)
+                                Text("17 kcal").padding(.trailing, 55)
+                                Text("2 gr")
+                            }.padding(.vertical, 0.4).bold().font(.system(size:17))
+                            
+                            Text("1 bowl")
+                        }
+                    }.frame(width: 356, height: 69.63).background(Color("bg_yellow")).cornerRadius(15)
+                    
+                    VStack{
+                        Text("For You").font(.system(size: 22)).padding(.trailing,260)
+                    }
                     HStack{
-                        VStack{
-                            Text("🫐🧀").padding(.top, 10).foregroundColor(.black).bold().font(.system(size: 20))
-                            
-                            Text("What have you eaten today? Let us track here!").foregroundColor(.black).font(.system(size: 18)).multilineTextAlignment(.center).padding(.top, 1)
-                            Button(
-                                action: {
-                                    
-                                }){
-                                    Text("Add your food")
-                                        .foregroundColor(.black).frame(width: 288, height: 46).background(Color("button_color")).cornerRadius(11)
-                                }.padding()
-                            
-                        }.padding(.trailing, 15)
-                        
+                        ZStack{
+                            VStack(alignment: .leading){
+                                Text("What is BMI?").bold().padding(.bottom, 9)
+                                Text("Do you know BMI is a measure if your weight is healthy or not.")
+                            }.padding()
+                        }.frame(width: 169, height: 186).background(Color("bg_pink")).cornerRadius(29)
+                        ZStack{
+                            VStack(alignment: .leading){
+                                Text("What is BMI?").bold().padding(.bottom, 9)
+                                Text("Do you know BMI is a measure if your weight is healthy or not.")
+                            }.padding()
+                        }.frame(width: 169, height: 186).background(Color("bg_blue")).cornerRadius(29)
                     }
+                   
                     
-                    
-                }.frame(width: 359, height: 186).background(Color("bg_yellow")).cornerRadius(29)
-                ZStack{
-                    VStack(alignment: .leading){
-                        HStack{
-                            Text("Bubur Ayam").padding(.trailing, 55)
-                            Text("17 kcal").padding(.trailing, 55)
-                            Text("2 gr")
-                        }.padding(.vertical, 0.4).bold().font(.system(size:17))
-                        
-                        Text("1 bowl")
-                    }
-                }.frame(width: 356, height: 69.63).background(Color("bg_yellow")).cornerRadius(15)
-                ZStack{
-                    VStack(alignment: .leading){
-                        HStack{
-                            Text("Bubur Ayam").padding(.trailing, 55)
-                            Text("17 kcal").padding(.trailing, 55)
-                            Text("2 gr")
-                        }.padding(.vertical, 0.4).bold().font(.system(size:17))
-                        
-                        Text("1 bowl")
-                    }
-                }.frame(width: 356, height: 69.63).background(Color("bg_yellow")).cornerRadius(15)
-                
-                VStack{
-                    Text("For You").font(.system(size: 22)).padding(.trailing,260)
-                }
-                HStack{
-                    ZStack{
-                        VStack(alignment: .leading){
-                            Text("What is BMI?").bold().padding(.bottom, 9)
-                            Text("Do you know BMI is a measure if your weight is healthy or not.")
-                        }.padding()
-                    }.frame(width: 169, height: 186).background(Color("bg_pink")).cornerRadius(29)
-                    ZStack{
-                        VStack(alignment: .leading){
-                            Text("What is BMI?").bold().padding(.bottom, 9)
-                            Text("Do you know BMI is a measure if your weight is healthy or not.")
-                        }.padding()
-                    }.frame(width: 169, height: 186).background(Color("bg_blue")).cornerRadius(29)
-                }
-               
-                
-            }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.white).foregroundColor(.black)
-        }
+                }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.white).foregroundColor(.black)
+            }
+        }.navigationBarTitle("Summary")
+        
     }
 }
 
