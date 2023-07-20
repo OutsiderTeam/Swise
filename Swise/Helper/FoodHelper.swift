fc6699a67f7bff39c26eaa156332ba4f1e16f29e
agfidprasetyo <agfid11@gmail.com>
2023-07-20 17:00:15 +0700

    change added sugar to sugar

--- Swise/Helper/FoodHelper.swift
+++ Swise/Helper/FoodHelper.swift
@@ -31,7 +31,7 @@ func addEatenFood(food: FoodDetail, index: Int, totalSugar: Double, totalCalorie
     newItem.eatenFoods = DataItem(context: viewContext)
     newItem.eatenFoods?.date = Date().formatted(date: .complete, time: .omitted)
     newItem.eatenFoods?.timestamp = Date()
-    newItem.eatenFoods?.totalSugar = totalSugar + (Double(food.servings.serving![index].addedSugars ?? "0") ?? 0)
+    newItem.eatenFoods?.totalSugar = totalSugar + (Double(food.servings.serving![index].sugar ?? "0") ?? 0)
     newItem.eatenFoods?.totalCalories = totalCalories + (Double(food.servings.serving![index].calories ?? "0") ?? 0)
     newItem.servingFood = ServingFood(context: viewContext)
     newItem.servingFood?.addedSugars = food.servings.serving?[index].addedSugars
