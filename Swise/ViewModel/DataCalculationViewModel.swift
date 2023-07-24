f83442300e8225ef2ee50f68045ce80ef5a88d14
ayulestariramadani <ayulestariramadani@gmail.com>
2023-07-24 16:29:16 +0700

    fix handler if some data is not filled

--- Swise/ViewModel/DataCalculationViewModel.swift
+++ Swise/ViewModel/DataCalculationViewModel.swift
@@ -26,6 +26,14 @@ class DataCalculationViewModel: ObservableObject{
     // Every 1 tea spoon sugar = 16 kkal
     let caloryTeaSpoon: Double = 16
     
+    func healthChecker()-> Bool{
+        if (height == 0 && age == 0 && (sex == "" || sex == "Other")){
+            return false
+        } else {
+            return true
+        }
+        
+    }
     // Function to calculate the user's calorie needs
     func calorieNeed() -> Double{
         // BBI : Berat Badan Ideal
@@ -34,34 +42,36 @@ class DataCalculationViewModel: ObservableObject{
         var BMR: Double = 0
         // calNeed: Kalori yang dibutuhkan tubuh berdasarkan exercise
         var calNeed: Double = 0
-        
-        switch sex{
-        // When user as a female
-        case "Female":
-            //1. Calculate BBI
-            BBI = (height-100)-((15*(height-100))/100)
-            //2. Calculate BMR
-            BMR = 665 + (9.6*BBI) + (1.8*height) - (4.7*age)
-            //3. Calculate base on user's exercise
-            calNeed = calculateByActivityIntensity(BMR: BMR)
-            return calNeed
-            
-        //When user as a male
-        case "Male":
-            //1. Calculate BBI
-            BBI = (height-100)-((10*(height-100))/100)
-            //2. Calculate BMR
-            BMR = 66 + (13.7*BBI) + (5*height) - (6.8*age)
-            //3. Calculate base on user's exercise
-            calNeed = calculateByActivityIntensity(BMR: BMR)
-            return calNeed
-            
-        //When user gak jelas
-        default:
-            print("Tentuin lu Male apa Female bang")
-            BBI = 0
-            return BBI
+        if healthChecker(){
+            switch sex{
+                // When user as a female
+            case "Female":
+                //1. Calculate BBI
+                BBI = (height-100)-((15*(height-100))/100)
+                //2. Calculate BMR
+                BMR = 665 + (9.6*BBI) + (1.8*height) - (4.7*age)
+                //3. Calculate base on user's exercise
+                calNeed = calculateByActivityIntensity(BMR: BMR)
+//                return calNeed
+                
+                //When user as a male
+            case "Male":
+                //1. Calculate BBI
+                BBI = (height-100)-((10*(height-100))/100)
+                //2. Calculate BMR
+                BMR = 66 + (13.7*BBI) + (5*height) - (6.8*age)
+                //3. Calculate base on user's exercise
+                calNeed = calculateByActivityIntensity(BMR: BMR)
+//                return calNeed
+                
+                //When user gak jelas
+            default:
+                print("Tentuin lu Male apa Female bang")
+                calNeed = 0
+//                return BBI
+            }
         }
+        return calNeed
     }
     
     // Function to define the calculation base on user exercise routine
@@ -84,12 +94,27 @@ class DataCalculationViewModel: ObservableObject{
     
     //Function to calculate max sugar intake
     func calculateMaxSugar()->Int{
-        return Int(calorieNeed()*0.1*gramTeaSpoon/caloryTeaSpoon)
+        var maxSugar: Int = 0
+        if calorieNeed() == 0{
+            maxSugar = 0
+        } else{
+            maxSugar = Int(calorieNeed()*0.1*gramTeaSpoon/caloryTeaSpoon)
+        }
+        return maxSugar
+        
+        
     }
     
     // Function to calculate sugar as a tea spoon
     func calculateTeaSpoonOfSugar(calorie:Double)->Int{
-        return Int(calorie*0.1/caloryTeaSpoon)
+        var teaSpoonSugar: Int = 0
+        if calorie == 0{
+            teaSpoonSugar = 0
+        } else {
+            teaSpoonSugar = Int(calorie*0.1/caloryTeaSpoon)
+        }
+        return teaSpoonSugar
+        
     }
     
 }
