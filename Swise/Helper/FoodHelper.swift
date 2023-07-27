7db2c0c6c283ef9c16d6def2be7dc5ea9ab66dac
agfidprasetyo <agfid11@gmail.com>
2023-07-28 04:19:59 +0700

    add food manually

--- Swise/Helper/FoodHelper.swift
+++ Swise/Helper/FoodHelper.swift
@@ -14,61 +14,70 @@ let viewContext = PersistenceController.shared.container.viewContext
 //********************************************************//
 
 func addEatenFood(food: FoodDetail, index: Int, totalSugar: Double, totalCalories: Double, sugarCondition: Double) {
-    let historyFood = HistoryFoods(context: viewContext)
-    historyFood.foodId = food.foodId
-    historyFood.brandName = food.brandName
-    historyFood.foodType = food.foodType
-    historyFood.foodUrl = food.foodUrl
-    historyFood.foodName = food.foodName
-    let newItem = EatenFoods(context: viewContext)
-    newItem.time = Date().formatted(date: .omitted, time: .shortened)
-    newItem.brandName = food.brandName
-    newItem.foodId = food.foodId
-    newItem.foodName = food.foodName
-    newItem.foodType = food.foodType
-    newItem.foodUrl = food.foodUrl
-    newItem.timestamp = Date()
-    newItem.eatenFoods = DataItem(context: viewContext)
-    newItem.eatenFoods?.date = Date().formatted(date: .complete, time: .omitted)
-    newItem.eatenFoods?.sugarCondition = sugarCondition
-    newItem.eatenFoods?.timestamp = Date()
-    newItem.eatenFoods?.totalSugar = totalSugar
-    newItem.eatenFoods?.totalCalories = totalCalories
-    newItem.servingFood = ServingFood(context: viewContext)
-    newItem.servingFood?.addedSugars = food.servings.serving?[index].addedSugars
-    newItem.servingFood?.calcium = food.servings.serving?[index].calcium
-    newItem.servingFood?.calories = food.servings.serving?[index].calories
-    newItem.servingFood?.carbohydrate = food.servings.serving?[index].carbohydrate
-    newItem.servingFood?.cholesterol = food.servings.serving?[index].cholesterol
-    newItem.servingFood?.fat = food.servings.serving?[index].fat
-    newItem.servingFood?.fiber = food.servings.serving?[index].fiber
-    newItem.servingFood?.iron = food.servings.serving?[index].iron
-    newItem.servingFood?.measurementDescription = food.servings.serving?[index].measurementDescription
-    newItem.servingFood?.metricServingAmount = food.servings.serving?[index].metricServingAmount
-    newItem.servingFood?.metricServingUnit = food.servings.serving?[index].metricServingUnit
-    newItem.servingFood?.monounsaturatedFat = food.servings.serving?[index].monounsaturatedFat
-    newItem.servingFood?.numberOfUnits = food.servings.serving?[index].numberOfUnits
-    newItem.servingFood?.polyunsaturatedFat = food.servings.serving?[index].polyunsaturatedFat
-    newItem.servingFood?.potassium = food.servings.serving?[index].potassium
-    newItem.servingFood?.protein = food.servings.serving?[index].protein
-    newItem.servingFood?.saturatedFat = food.servings.serving?[index].saturatedFat
-    newItem.servingFood?.servingDescription = food.servings.serving?[index].servingDescription
-    newItem.servingFood?.servingId = food.servings.serving?[index].servingId
-    newItem.servingFood?.servingUrl = food.servings.serving?[index].servingUrl
-    newItem.servingFood?.sodium = food.servings.serving?[index].sodium
-    newItem.servingFood?.sugar = food.servings.serving?[index].sugar
-    newItem.servingFood?.transFat = food.servings.serving?[index].transFat
-    newItem.servingFood?.vitaminA = food.servings.serving?[index].vitaminA
-    newItem.servingFood?.vitaminC = food.servings.serving?[index].vitaminC
-    newItem.servingFood?.vitaminD = food.servings.serving?[index].vitaminD
-    
+    let i = index >= 0 ? index : 0
+    if index >= 0 {
+        let historyFood = HistoryFoods(context: viewContext)
+        historyFood.foodId = food.foodId
+        historyFood.brandName = food.brandName
+        historyFood.foodType = food.foodType
+        historyFood.foodUrl = food.foodUrl
+        historyFood.foodName = food.foodName
+    }
+        let newItem = EatenFoods(context: viewContext)
+        newItem.time = Date().formatted(date: .omitted, time: .shortened)
+        newItem.brandName = food.brandName
+        newItem.foodId = food.foodId
+        newItem.foodName = food.foodName
+        newItem.foodType = food.foodType
+        newItem.foodUrl = food.foodUrl
+        newItem.timestamp = Date()
+        newItem.eatenFoods = DataItem(context: viewContext)
+        newItem.eatenFoods?.date = Date().formatted(date: .complete, time: .omitted)
+        newItem.eatenFoods?.sugarCondition = sugarCondition
+        newItem.eatenFoods?.timestamp = Date()
+        newItem.eatenFoods?.totalSugar = totalSugar
+        newItem.eatenFoods?.totalCalories = totalCalories
+        newItem.servingFood = ServingFood(context: viewContext)
+        newItem.servingFood?.addedSugars = food.servings.serving?[i].addedSugars
+        newItem.servingFood?.calcium = food.servings.serving?[i].calcium
+        newItem.servingFood?.calories = food.servings.serving?[i].calories
+        newItem.servingFood?.carbohydrate = food.servings.serving?[i].carbohydrate
+        newItem.servingFood?.cholesterol = food.servings.serving?[i].cholesterol
+        newItem.servingFood?.fat = food.servings.serving?[i].fat
+        newItem.servingFood?.fiber = food.servings.serving?[i].fiber
+        newItem.servingFood?.iron = food.servings.serving?[i].iron
+        newItem.servingFood?.measurementDescription = food.servings.serving?[i].measurementDescription
+        newItem.servingFood?.metricServingAmount = food.servings.serving?[i].metricServingAmount
+        newItem.servingFood?.metricServingUnit = food.servings.serving?[i].metricServingUnit
+        newItem.servingFood?.monounsaturatedFat = food.servings.serving?[i].monounsaturatedFat
+        newItem.servingFood?.numberOfUnits = food.servings.serving?[i].numberOfUnits
+        newItem.servingFood?.polyunsaturatedFat = food.servings.serving?[i].polyunsaturatedFat
+        newItem.servingFood?.potassium = food.servings.serving?[i].potassium
+        newItem.servingFood?.protein = food.servings.serving?[i].protein
+        newItem.servingFood?.saturatedFat = food.servings.serving?[i].saturatedFat
+        newItem.servingFood?.servingDescription = food.servings.serving?[i].servingDescription
+        newItem.servingFood?.servingId = food.servings.serving?[i].servingId
+        newItem.servingFood?.servingUrl = food.servings.serving?[i].servingUrl
+        newItem.servingFood?.sodium = food.servings.serving?[i].sodium
+        newItem.servingFood?.sugar = food.servings.serving?[i].sugar
+        newItem.servingFood?.transFat = food.servings.serving?[i].transFat
+        newItem.servingFood?.vitaminA = food.servings.serving?[i].vitaminA
+        newItem.servingFood?.vitaminC = food.servings.serving?[i].vitaminC
+        newItem.servingFood?.vitaminD = food.servings.serving?[i].vitaminD
+//    } else {
+//        let newItem = EatenFoods(context: viewContext)
+//        newItem.time = Date().formatted(date: .omitted, time: .shortened)
+//        newItem.foodName = food.foodName
+//        newItem.foodId = food.foodId
+//        newItem.
+//    }
     do {
         try viewContext.save()
     } catch {
         // Replace this implementation with code to handle the error appropriately.
         // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        print("Unresolved error \(nsError), \(nsError.userInfo)")
     }
     
 }
