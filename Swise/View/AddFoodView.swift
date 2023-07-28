370d6efdf5207061636cc89a84af02c755d49238
agfidprasetyo <agfid11@gmail.com>
2023-07-28 15:48:50 +0700

    add history food at search

--- Swise/View/AddFoodView.swift
+++ Swise/View/AddFoodView.swift
@@ -20,6 +20,12 @@ struct AddFoodView: View {
         sortDescriptors: [],
         animation: .default)
     private var items: FetchedResults<DataItem>
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \HistoryFoods.foodName, ascending: true)],
+        animation: .default)
+    private var historyFoods: FetchedResults<HistoryFoods>
+    
     var calNeed: Double = 0
     @State var data: [EatenFoods] = []
     
@@ -30,40 +36,51 @@ struct AddFoodView: View {
                 ZStack{
                     CustomNavBarContainerView(isSearch: true){
                         if text == "" {
-                            VStack(alignment: .leading){
-                                Text("Your recent search").font(.headline).padding()
-                                    if data.count > 0 {
+                            VStack {
+                                VStack(alignment: .leading){
+                                    Text("Your history food")
+                                        .font(.headline)
+                                        .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
+                                    if historyFoods.count > 0 {
                                         List{
-                                            ForEach(0..<(data.count <= 5 ? data.count : 5), id: \.self) { i in
-                                                
+                                            ForEach(0..<(historyFoods.count <= 5 ? historyFoods.count : 5), id: \.self) { i in
                                                 HStack{
-                                                    Text("\(data[i].foodName!)")
+                                                    Text("\(historyFoods[i].foodName!)")
                                                     Spacer()
                                                     Image(systemName: "chevron.right")
                                                 }
+                                                .onTapGesture {
+                                                    viewModel.getFood(id: Int(historyFoods[i].foodId!)!)
+                                                    isPresented = true
+                                                }
                                                 .padding()
-                                                .listRowBackground(Color("bg_yellow").cornerRadius(10).padding(3))
+                                                .listRowBackground(Color("bg_yellow")
+                                                .cornerRadius(10)
+                                                .padding(3))
                                             }.listRowSeparator(.hidden)
                                         }.scrollContentBackground(.hidden)
-                                            .frame(maxHeight: .infinity)
                                     } else {
                                         EmptyListView()
                                     }
-                            }
-                            
-                            
-                            Button(
-                                action: {
-                                    foodManual = true
-                                }){
-                                    HStack{
-                                        Image(systemName: "plus.circle")
-                                        Text("Add your food manually")
-                                            .font(.headline)
-                                            .fontWeight(.semibold)
-                                    }.foregroundColor(.white).frame(width: 358, height: 50).background(Color("button_color")).cornerRadius(11)
-                                    
                                 }
+                                
+                                Button(
+                                    action: {
+                                        foodManual = true
+                                    }){
+                                        HStack{
+                                            Image(systemName: "plus.circle")
+                                            Text("Add your food manually")
+                                                .font(.headline)
+                                                .fontWeight(.semibold)
+                                        }
+                                        .foregroundColor(.white)
+                                        .frame(width: 358, height: 50)
+                                        .background(Color("button_color"))
+                                        .cornerRadius(11)
+                                        
+                                    }.padding(.bottom, 20)
+                            }.frame(maxHeight: .infinity)
                         } else {
                             ZStack{
                                 Color.white
@@ -71,12 +88,12 @@ struct AddFoodView: View {
                                     List {
                                         ForEach(viewModel.resultSearch, id: \.foodId) { food in
                                             HStack{
-                                                Text(food.foodName).onTapGesture {
-                                                    viewModel.getFood(id: Int(food.foodId)!)
-                                                    isPresented = true
-                                                }
+                                                Text(food.foodName)
                                                 Spacer()
                                                 Image(systemName: "chevron.right")
+                                            }.onTapGesture {
+                                                viewModel.getFood(id: Int(food.foodId)!)
+                                                isPresented = true
                                             }
                                             .padding()
                                             .frame(width: 361, height: 47)
@@ -87,9 +104,6 @@ struct AddFoodView: View {
                                     
                                     
                                     .scrollContentBackground(.hidden)
-                                    .navigationDestination(isPresented: $isPresented) {
-                                        FoodInformationView(maxSugar: $maxSugar, totalSugar: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalSugar ?? 0, totalCalories: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalCalories ?? 0, calNeed: calNeed)
-                                    }
                                 }
                             }
                         }
@@ -103,18 +117,21 @@ struct AddFoodView: View {
                 
             }
         }
-            .navigationTitle("Add Food")
+        .navigationTitle("Add Food")
+        .navigationDestination(isPresented: $isPresented) {
+            FoodInformationView(maxSugar: $maxSugar, totalSugar: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalSugar ?? 0, totalCalories: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalCalories ?? 0, calNeed: calNeed)
+        }
+        
+        .navigationDestination(isPresented: $foodManual) {
+            AddNewFoodView(maxSugar: $maxSugar, totalSugar: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalSugar ?? 0, totalCalories: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalCalories ?? 0, calNeed: calNeed)
+        }
+        .onChange(of: text, perform: { newValue in
             
-            .navigationDestination(isPresented: $foodManual, destination: {
-                AddNewFoodView(maxSugar: $maxSugar, totalSugar: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalSugar ?? 0, totalCalories: items.isEmpty ? 0 : items.filter {$0.date == Date().formatted(date: .complete, time: .omitted)}.first?.totalCalories ?? 0, calNeed: calNeed)
-            })
-            .onChange(of: text, perform: { newValue in
-                
-                viewModel.searchFood(query: text)
-            })
-            .onAppear{
-                data = items.first?.eatenFoodsArray ?? []
-            }
+            viewModel.searchFood(query: text)
+        })
+        .onAppear{
+            data = items.first?.eatenFoodsArray ?? []
+        }
         
     }
 }
